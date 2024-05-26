5b6d77b643dcac4c4702e690ff46e88f87a4495d
Vikki <vikas@hood.live>
2024-05-26 19:24:01 +0530

    Base website design

---
+++ src/constants/trusted-partners.constant.ts
@@ -0,0 +1,27 @@
+export const TrustedPartners = [
+  {
+    title: "YC",
+    logoSrc: "/icons/ycombinator.svg",
+    color: "#ffffff",
+  },
+  {
+    title: "American Express",
+    logoSrc: "/icons/americanexpress.svg",
+    color: "#ffffff",
+  },
+  {
+    title: "Adobe",
+    logoSrc: "/icons/adobe.svg",
+    color: "#ffffff",
+  },
+  {
+    title: "DC",
+    logoSrc: "/icons/dcentertainment.svg",
+    color: "#ffffff",
+  },
+  {
+    title: "WarnerBros",
+    logoSrc: "/icons/warnerbros.svg",
+    color: "#ffffff",
+  },
+];
